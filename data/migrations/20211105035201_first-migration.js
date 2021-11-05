dad85bad82746730278f0d90104b8844947b138f
monicasalas86 <monicsalas167@gmail.com>
2021-11-04 23:53:31 -0500

    finished steps table

--- data/migrations/20211105035201_first-migration.js
+++ data/migrations/20211105035201_first-migration.js
@@ -2,11 +2,25 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('recipes', table => {
             table.increments('recipe_id')
-            table.string('recipe_name', 128).notNullable().unique()
+            table.string('recipe_name', 128)
+                .notNullable()
+                .unique()
         })
         .createTable('steps', table => {
             table.increments('step_id')
-            table.string('step_number', 128)
+            table.integer('step_number')
+                .notNullable()
+                .unsigned()
+            table.string('step_instructions', 500)
+                .notNullable()
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
+            
         })
         .createTable('ingredients', table => {
             table.increments('ingredient_id')
