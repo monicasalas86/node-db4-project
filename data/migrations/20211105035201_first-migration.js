bdf27da185d352403b94169072a24cff65b1bdc5
monicasalas86 <monicsalas167@gmail.com>
2021-11-05 00:02:01 -0500

    finished migrating tables

--- data/migrations/20211105035201_first-migration.js
+++ data/migrations/20211105035201_first-migration.js
@@ -24,12 +24,37 @@ exports.up = function(knex) {
         })
         .createTable('ingredients', table => {
             table.increments('ingredient_id')
-            table.string('ingredient_name', 128)
+            table.string('ingredient_name', 128).notNullable()
+        })
+        .createTable('step_ingredients', table => {
+            table.increments('step_ingredient_id')
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
+            table.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
+            table.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
         })
 };
 
 exports.down = function(knex) {
     return knex.schema
+        .dropTableIfExists('step_ingredients')
         .dropTableIfExists('ingredients')
         .dropTableIfExists('steps')
         .dropTableIfExists('recipes')
